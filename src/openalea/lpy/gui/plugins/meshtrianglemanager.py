5bb6ee0e035652e7d2133e62e2db88faa0006ce8
Jonathan LEVY <23365238+j-levy@users.noreply.github.com>
2021-06-10 22:22:22 +0200

    load file with ObjLoader - maybe change manager name?

--- src/openalea/lpy/gui/plugins/meshtrianglemanager.py
+++ src/openalea/lpy/gui/plugins/meshtrianglemanager.py
@@ -1,12 +1,15 @@
-#try:
-#    from openalea.plantgl.gui.curve2deditor import Curve2DEditor, FuncConstraint
-#except ImportError as e:
-#    Curve2DEditor = None
+
+from openalea.plantgl.gui.objloader import ObjLoader
 
 from openalea.lpy.gui.abstractobjectmanager import *
 from openalea.plantgl.gui.qt import QtGui, QtWidgets
 
+from openalea.plantgl.all import Scene, Polyline
+
 import openalea.plantgl.all as pgl
+from openalea.plantgl.gui.qt.QtWidgets import QFileDialog
+from openalea.plantgl.codec.obj import codec as obj_codec
+from openalea.plantgl.codec.obj import Group
 
 
 class MeshTriangleManager(AbstractPglObjectManager):
@@ -16,13 +19,49 @@ class MeshTriangleManager(AbstractPglObjectManager):
         AbstractPglObjectManager.__init__(self, "MeshTriangle")
 
     def displayThumbnail(self, obj, i, focus, objectthumbwidth):
-        raise NotImplementedError('displayThumbnail')
-        # displayLineAsThumbnail(self, obj, i, objectthumbwidth, (1, 0, 1, 1))
+        # raise NotImplementedError('displayThumbnail')
+        return None
+
+    # TODO: check if editor functions are relevant for this use case (is there a 3D editor in PlantGL for meshes? And should we use it?)
+    def getEditor(self, parent_widget):
+        """ ask for creation of editor. Should be reimplemented """
+        # raise NotImplementedError('getEditor')
+        return ObjLoader(parent_widget)
+
+    def fillEditorMenu(self, menubar, editor):
+        """ Function call to fill the menu of the editor """
+        menu = QtWidgets.QMenu('File', menubar)
+        menu.addAction('Load .obj', lambda: self.loadObj(editor))
+        menubar.addMenu(menu)
+
+    def loadObj(self, editor):
+        fname = QFileDialog.getOpenFileName(editor, 'Open file',
+            '~',"Obj files (*.obj)")
+        print(fname)
+        fname = fname[0]
+        scene = obj_codec.read(fname)
+        scene.name = ""
+        self.setObjectToEditor(editor, scene)
+
+    def setObjectToEditor(self, editor, obj):
+        """ ask for edition of obj with editor. Should be reimplemented """
+        # raise NotImplementedError('setObjectToEditor')
+        editor.display(obj)
+
+    def retrieveObjectFromEditor(self, editor):
+        """ ask for current value of object being edited """
+        # raise NotImplementedError('startObjectEdition')
+        return editor.scene
+
+    def defaultObjectTypes(self):
+        """ ask for type of object managed by this manager. Several are possible. None means that typename should be used. """
+        return None
 
     def createDefaultObject(self, subtype=None):
         import math
+        scene = pgl.Scene()
         # list of points
-        points = [(1, 0, -1 / math.sqrt(2)),
+        vertices = [(1, 0, -1 / math.sqrt(2)),
                   (-1, 0, -1 / math.sqrt(2)),
                   (0, 1, 1 / math.sqrt(2)),
                   (0, -1, 1 / math.sqrt(2))]
@@ -32,28 +71,17 @@ class MeshTriangleManager(AbstractPglObjectManager):
                         (0, 1, 3),
                         (1, 2, 3),
                         (0, 2, 3)]  #
-        tetra = pgl.TriangleSet(points, connectivity)
-        return tetra
+        s = Group('').shape(vertices=vertices, normals=connectivity, textures=[])
+        scene.add(s)
+        # FIXME: this doesn't work, I don't know why yet
+        return scene
 
-    # TODO: check if editor functions are relevant for this use case (is there a 3D editor in PlantGL for meshes? And should we use it?)
-    """
-    def getEditor(self, parent):
-        return None #no given editor for TriangleSet.
+    def writeObject(self, obj, indentation):
+        raise NotImplementedError('writeObject')
 
-    def setObjectToEditor(self, editor, obj):
-        # ask for edition of obj with editor
-        from copy import deepcopy
-        editor.setCurve(deepcopy(obj))
-
-    def retrieveObjectFromEditor(self, editor):
-        # ask for current value of object being edited
-        return editor.getCurve()
-    """
     def writeObjectToLsysContext(self, obj):
         itemName = "" # find a name somehow, could be random
         code = f'''
-import openalea.plantgl.all as pgl\n
-
 points{itemName} = {self.pointsToPython(obj)}\n
 index{itemName} = {self.indexToPython(obj)}\n
 {itemName} = pgl.TriangleSet(points{itemName}, index{itemName})\n
@@ -61,31 +89,29 @@ index{itemName} = {self.indexToPython(obj)}\n
         return code
 
     def canImportData(self, fname):
-        ## TODO: rewrite this to load some 3D object file (I'll see what's easy to load)
-        """
         from os.path import splitext
         ext = splitext(fname)[1]
-        return ext == '.fset' or ext == '.func'
-        """
+        return ext == '.obj'
 
     def importData(self, fname):
-        ## TODO: rewrite this to load some 3D object file (I'll see what's easy to load)
-        """
-        from openalea.lpy.gui.lpfg_data_import import import_functions, import_function
-        from os.path import splitext
-        ext = splitext(fname)[1]
-        if ext == '.fset':
-            return import_functions(fname)
-        else:
-            return import_function(fname)
-        """
+        from openalea.plantgl.codec.obj import codec
+        if self.canImportData(fname):
+            return codec.read(fname)
 
-    def fillEditorMenu(self, menubar, editor):
-        """ Function call to fill the menu of the editor """
-        menu = QtWidgets.QMenu('Theme', menubar)
-        menu.addAction('Black', lambda: editor.applyTheme(editor.BLACK_THEME))
-        menu.addAction('White', lambda: editor.applyTheme(editor.WHITE_THEME))
-        menubar.addMenu(menu)
+    def completeContextMenu(self, menu, obj, widget):
+        pass
+
+    def managePrimitive(self):
+        return False
+
+    def getTheme(self):
+        """ get the color theme currently used """
+        return {}
+
+    def setTheme(self, theme):
+        """ get the color theme according to the theme dict """
+        pass
+
 
     ### private functions
 
